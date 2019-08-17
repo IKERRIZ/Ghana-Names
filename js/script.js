d16e6809cb4973f2d831f2b4f18c1034ca3e11e1
IKERRIZ <okothfaith94@gmail.com>
2019-08-17 12:48:35 +0300

    add third page structure

---
+++ js/script.js
@@ -0,0 +1,28 @@
+// validate the form
+// var akannames =[
+//   male{
+//  Sunday:Kwasi;
+//  Monday: Kwadwo;
+//  Tuesday:Kwabena;
+//  Wednesday:Kwaku;
+//  Thursday:Yaw;
+//  Friday:Kofi;
+//  Saturday:Kwame;
+//  }
+//  ]
+// var akannames =[
+//    female{
+//  Sunday:Akosua;
+//  Monday:Adwoa;
+//  Tuesday: Abenaa;
+//  Wednesday:Akua;
+//  Thursday: Yaa;
+//  Friday:Afua;
+//  Saturday:Ama; 
+
+//}
+//]
+
+
+ 
+ 
