cef637683bca6da3342d20e745457fc0d94606e7
IKERRIZ <okothfaith94@gmail.com>
2019-08-18 23:38:11 +0300

    add form validation to script.js

--- js/script.js
+++ js/script.js
@@ -1,30 +1,41 @@
-// variables
-// var akannames =[
-//   male{
-//  Sunday:Kwasi;
-//  Monday: Kwadwo;
-//  Tuesday:Kwabena;
-//  Wednesday:Kwaku;
-//  Thursday:Yaw;
-//  Friday:Kofi;
-//  Saturday:Kwame;
-//  }
-//  ]
-// var akannames =[
-//    female{
-//  Sunday:Akosua;
-//  Monday:Adwoa;
-//  Tuesday: Abenaa;
-//  Wednesday:Akua;
-//  Thursday: Yaa;
-//  Friday:Afua;
-//  Saturday:Ama; 
 
-//}
-//]
-function validateForm(){
- var
+var maleList = [
+    {
+        Sunday: "Kwasi",
+        Monday: "Kwadwo",
+        Tuesday: "Kwabena",
+        Wednesday: "Kwaku",
+        Thursday: "Yaw",
+        Friday: "Kofi",
+        Saturday: "Kwame"
+    }
+];
+var femyList = [
+    {
+        Sunday: "Akosua",
+        Monday: "Adwoa",
+        Tuesday: "Abenaa",
+        Wednesday: "Akua",
+        Thursday: "Yaa",
+        Friday: "Afua",
+        Saturday: "Ama"
+
+    }
+];
+
+function validateForm () {
+    var day = document.getElementById("day");
+    var month = document.getElementById("month");
+    var year = document.getElementById("year");
+
+    if (day <=0 || day > 31){
+        alert("Invalid day");
+        
+    }
+
+   
 }
 
- 
- 
+
+
+
